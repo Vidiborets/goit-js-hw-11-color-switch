73093c931fe54fe1f617f519fb96cef269ca6e70
Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2021-09-03 13:12:16 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import './sass/main.scss';
 import './js/createTheme';
-import './js/createElements';
--- src/js/createElements.js
+++
@@ -1,7 +0,0 @@
-import elementFood from '../js/menu.json';
-import createElement from '../tamplate/tamplate.hbs';
-
-const importGallary = document.querySelector('.js-menu')
-
-const item = createElement(elementFood);
-importGallary.insertAdjacentHTML('beforeend', item);
